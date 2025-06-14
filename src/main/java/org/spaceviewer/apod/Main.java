9dad65aaa9bc24d7103f7a09284c862fa176c062
Davi Santos <davi.santos4357@gmail.com>
2025-06-14 18:30:00 -0300

    feat: add Main class

--- src/main/java/org/spaceviewer/apod/Main.java
+++ src/main/java/org/spaceviewer/apod/Main.java
@@ -2,6 +2,8 @@ package org.spaceviewer.apod;
 
 public class Main {
     public static void main(String[] args) {
-
+        for (String arg : args) {
+            System.out.println("Received argument: " + arg);
+        }
     }
 }
