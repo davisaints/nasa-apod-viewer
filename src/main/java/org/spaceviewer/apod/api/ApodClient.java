801ae6c8e2e27b3913f7240eb4f6922e40b39680
Davi Santos <davi.santos4357@gmail.com>
2025-06-18 21:14:21 -0300

    refactor: remove external dotenv library and load API key from local config.properties

--- src/main/java/org/spaceviewer/apod/api/ApodClient.java
+++ src/main/java/org/spaceviewer/apod/api/ApodClient.java
@@ -3,13 +3,15 @@ package org.spaceviewer.apod.api;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
+import java.io.InputStream;
 import java.io.IOException;
+
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.URI;
+
+import java.util.Properties;
 
 import org.spaceviewer.apod.model.ApodResponse;
 
@@ -19,11 +21,8 @@ public class ApodClient {
     }
 
     public ApodResponse fetchApodData() throws IOException, InterruptedException {
-        Dotenv dotenv = Dotenv.load();
-        String apiKey = dotenv.get("API_KEY");
-
         String APOD_API_URL = "https://api.nasa.gov/planetary/apod";
-        URI uri = URI.create(APOD_API_URL + "?api_key=" + apiKey);
+        URI uri = URI.create(APOD_API_URL + "?api_key=" + _getProperty("api.key"));
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(uri)
@@ -36,6 +35,23 @@ public class ApodClient {
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         return objectMapper.readValue(response.body(), ApodResponse.class);
+    }
+
+    private String _getProperty(String propertyKey) throws IOException {
+        InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties");
+        if (input == null) {
+            throw new IOException("config.properties not found in classpath.");
+        }
+
+        Properties props = new Properties();
+        props.load(input);
+
+        String apiKey = props.getProperty(propertyKey);
+        if (apiKey == null || apiKey.isBlank()) {
+            throw new IllegalStateException(propertyKey + " not set in config.properties");
+        }
+
+        return apiKey;
     }
 
     private final HttpClient httpClient;
